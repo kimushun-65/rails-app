69c4bd0e2801e8e4b4abf7625cdcdf45bdf35c05
shunsuke <kimushun1226.634@gmail.com>
2025-05-07 21:21:55 +0900

    新規作成ページの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,23 @@ class BoardsController < ApplicationController
   def index
     @boards = []  # 将来的にはBoard.allなどに置き換える
   end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to boards_path, notice: '掲示板を作成しました'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:author_name, :title, :body)
+  end
 end
