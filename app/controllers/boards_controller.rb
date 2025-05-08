ebcd840dc166987d853a844b493c76cd7de69e99
shunsuke <kimushun1226.634@gmail.com>
2025-05-08 14:10:29 +0900

    layout

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -33,6 +33,12 @@ class BoardsController < ApplicationController
     end
   end
 
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path, notice: '掲示板を削除しました'
+  end
+
   private
 
   def board_params
