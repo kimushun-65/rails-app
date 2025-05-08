89b9ed85063cd3139e99a6a2ff242c0d05594062
shunsuke <kimushun1226.634@gmail.com>
2025-05-08 10:53:07 +0900

    編集ページの作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -20,6 +20,19 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+    if @board.update(board_params)
+      redirect_to @board, notice: '掲示板を更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
 
   def board_params
