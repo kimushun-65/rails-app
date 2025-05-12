1a8aaaa37a8ad704a833b1b38e97c93178f444cc
shunsuke <kimushun1226.634@gmail.com>
2025-05-13 07:18:02 +0900

    commentsの追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,16 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :integer          not null, primary key
+#  title       :string(30)       not null
+#  body        :text             not null
+#  author_name :string(20)       not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Board < ApplicationRecord
+  has_many : comments
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :author_name, presence: true, length: { maximum: 20 }
