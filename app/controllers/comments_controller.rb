889dbfc7668a1415a8e4877d0fbb3f079505c3ba
shunsuke <kimushun1226.634@gmail.com>
2025-05-13 07:41:32 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,19 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to board_path(@comment.board), notice: 'コメントを投稿しました'
+    else
+      redirect_to board_path(@comment.board), alert: 'コメントの投稿に失敗しました'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :comment, :board_id)
   end
 end
