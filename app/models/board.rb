45b4e735a9d216e3bfdb4f28114c3b7f7e5c1f82
shunsuke <kimushun1226.634@gmail.com>
2025-05-29 07:58:02 +0900

    board tag テーブルの作成

--- app/models/board.rb
+++ app/models/board.rb
@@ -11,6 +11,9 @@
 #
 class Board < ApplicationRecord
   has_many :comments
+  has_many :board_tag_relations
+  has_many :tags, through: :board_tag_relations
+  
   validates :title, presence: true, length: { maximum: 30 }
   validates :body, presence: true, length: { maximum: 1000 }
   validates :author_name, presence: true, length: { maximum: 20 }
