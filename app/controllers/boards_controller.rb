964d171677a768d03820b280c7cc6343fad9ded2
shunsuke <kimushun1226.634@gmail.com>
2025-05-29 08:14:54 +0900

    タグで絞り込み

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,7 +2,8 @@ class BoardsController < ApplicationController
   before_action :set_target_board, only: [:show, :edit, :update, :destroy]
 
   def index
-    @boards =Board.page(params[:page]).per(10)
+    @boards = params[:tag_id].present? ? Tag.find(params[:tag_id]).boards : Board.all
+    @boards = @boards.page(params[:page]).per(10)
   end
 
   def new
